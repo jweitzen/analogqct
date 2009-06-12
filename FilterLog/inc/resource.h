4a939da1f99573336ed0ab988415227ac8f892b0
giulio.inbox <giulio.inbox@6d061dfa-575b-11de-9a1b-6103e4980ab8>
2009-06-12 15:28:32 +0000

    Added a file remotely

---
+++ FilterLog/inc/resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by FilterLog.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDC_IPEDIT                      101
+#define IDR_MAINFRAME                   128
+#define IDR_FILTERTYPE                  129
+#define IDD_FILTER_INFO                 130
+#define IDC_FILTER_NAME                 1000
+#define IDC_CHIPSET                     1001
+#define IDC_PHONE_MODEL                 1002
+#define IDC_AUTHOR                      1004
+#define IDC_DATE_MODIFIED               1005
+#define IDC_COMMENTS                    1006
+#define IDC_SW_VERSION                  1007
+#define IDC_QCAT_FILTERS                1008
+#define ID_VIEW_FILTERINFORMATION       32771
+#define ID_EDIT_CHECKALL                32775
+#define ID_EDIT_UNCHECKALL              32776
+#define ID_BUTTON32780                  32780
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32781
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
