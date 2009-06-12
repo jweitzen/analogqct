8a17e4aa7557bbcc2912eaf83c195d5c0517c934
giulio.inbox <giulio.inbox@6d061dfa-575b-11de-9a1b-6103e4980ab8>
2009-06-12 14:32:38 +0000

    Added a file remotely

---
+++ trunk/inc/resource.h
@@ -0,0 +1,53 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by AnaLog.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_ANALOGTYPE                  129
+#define IDD_FILTERING_OPTIONS           130
+#define IDD_SETTINGS                    131
+#define IDC_QCAT_FILTERS                1000
+#define IDC_STRING_FILTERS              1001
+#define IDC_CASE_SENSITIVE              1002
+#define IDC_RRC_HTML_PATH               1003
+#define IDC_BUTTON_RRC_HTML             1004
+#define IDC_RADIO_MSLOG                 1005
+#define IDC_RADIO_QXDM                  1006
+#define IDC_RADIO_VFRAMES               1010
+#define IDC_RADIO_HFRAMES               1011
+#define IDC_EDIT_FILTERS_PATH           1012
+#define IDC_BUTTON_FILTERS              1013
+#define IDC_FRAME_SPLIT                 1016
+#define IDC_SHOW_CONFIRM_MSG            1017
+#define IDC_QCAT_4                      1030
+#define IDC_QCAT_5                      1031
+#define IDC_HTML_LAYOUT                 1038
+#define IDC_QCAT_VERS                   1039
+#define IDC_QCAT4                       1045
+#define IDC_QCAT5                       1046
+#define IDC_HTML_Vert                   1047
+#define IDC_HTML_Horz                   1048
+#define IDC_QCAT6                       1049
+#define IDC_QCAT7                       1050
+#define IDC_APEX5                       1051
+#define IDC_APEX6                       1052
+#define IDC_APEX7                       1053
+#define ID_GO_BACK                      32771
+#define ID_ANALYSE_ANALYSELOG           32772
+#define ID_GO_FORWARD                   32773
+#define ID_ANALYSE_ANALYSEMSLOG         32779
+#define ID_VIEW_SETTINGS                32781
+#define ID_FILTERS_EDITFILTERS          32784
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32788
+#define _APS_NEXT_CONTROL_VALUE         1049
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
